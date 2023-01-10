f873a140a93bdc1a4b9bc0b4d8b433b5f9b3ad35
ZJW-92 <zjwei0201@gmail.com>
2023-01-10 09:44:59 +0100

    add header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  
+  return (
+    <div className="container mx-auto px-10 mb-10"> 
+      <div className="w-full inline-block py-10">
+        <div className="md:float-left block">
+          <Link href="/"> 
+            <span className="cursor-pointer font-bold text-6xl text-white">Optimal Recipe Blog</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-4 text-white ml-6 text-xl font-bold cursor-pointer">
+              {category.name}</span></Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
