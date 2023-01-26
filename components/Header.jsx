5e91d45985f6f2197ec065a9b74a566347054eaa
ZJW-92 <zjwei0201@gmail.com>
2023-01-26 14:10:54 +0100

    update header

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,14 +19,14 @@ const Header = () => {
       <div className="w-full inline-block py-10">
         <div className="md:float-left block">
           <Link href="/"> 
-            <span className="cursor-pointer font-bold text-6xl text-white">Optimal Recipe Blog</span>
+            <span className="cursor-pointer font-bold text-6xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-pink-300">Optimal Recipe Blog</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-4 text-white ml-6 text-xl font-bold cursor-pointer">
+              <span className="md:float-right mt-4 text-white ml-6 text-xl font-bold cursor-pointer ">
               {category.name}</span></Link>
           ))}
         </div>
